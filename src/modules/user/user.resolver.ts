369c7862ac5504fb45f71b631f886caf8cc31892
Bruce-CodeMan <1033684650@qq.com>
2023-03-31 16:22:01 +0800

    add update api

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -12,13 +12,26 @@ import { UserService } from "./user.service";
 export class UserResolver {
     constructor(private readonly userService: UserService){}
 
+    // 创建一个用户
     @Mutation(() => Boolean)
-    async create(@Args('params') params: UserInput): Promise<boolean> {
+    async create(
+        @Args('params'
+    ) params: UserInput): Promise<boolean> {
         return await this.userService.create(params);
     }
 
+    // 通过ID查询一个用户
     @Query(() => UserType, { description: "通过 ID 查询用户" })
     async find(@Args('id') id: string): Promise<UserType> {
         return await this.userService.findOne(id);
+    }
+
+    // 通过ID更新一个用户
+    @Mutation(() => Boolean, { description: "通过 ID 更新用户" })
+    async update(
+        @Args('id') id: string,
+        @Args('params') params: UserInput
+    ): Promise<boolean> {
+        return await this.userService.update(id, params);
     }
 }
