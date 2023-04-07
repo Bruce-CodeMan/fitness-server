2cfd1bf8ea6ad97d1a55843898f81d30671fa843
Bruce-CodeMan <1033684650@qq.com>
2023-04-07 10:00:19 +0800

    add page type

--- src/common/dto/page.type.ts
+++ src/common/dto/page.type.ts
@@ -1,5 +1,18 @@
+/*
+ * @Date: 2023-04-07 09:13:09
+ * @Author: Bruce
+ * @Description: 
+ */
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+
+@ObjectType()
 export class Page {
+    @Field(() => Int)
     total: number;
+
+    @Field(()=>Int)
     start?: number;
+
+    @Field(()=>Int)
     length?: number;
 }
--- src/common/dto/result.type.ts
+++ src/common/dto/result.type.ts
@@ -1,3 +1,12 @@
+/*
+ * @Date: 2023-04-07 09:13:09
+ * @Author: Bruce
+ * @Description: 
+ */
+import { Page } from './page.type';
+import { ClassType } from 'type-graphql'; 
+import { Field, Int, ObjectType } from "@nestjs/graphql";
+
 interface IResult<T> {
     code: number;
     message: string;
@@ -8,4 +17,44 @@ interface IResults<T> {
     code: number;
     message: string;
     data?: T[];
+    page?: Page;
+}
+
+export function createResult<T>(
+    ItemType: ClassType<T>
+): ClassType<IResult<T>> {
+
+    @ObjectType()
+    class Result {
+      @Field(() => Int)
+      code: number;
+
+      @Field(() => String)
+      message: string;
+
+      @Field(() => ItemType, { nullable: true })
+      data?: T;
+    }
+    return Result;
+  }
+
+export function createResults<T>(
+    ItemTypes:ClassType<T>
+): ClassType<IResults<T>> {
+
+    @ObjectType()
+    class Results {
+        @Field(()=>Int)
+        code: number;
+
+        @Field(()=>String)
+        message: string;
+
+        @Field(()=>[ItemTypes], {nullable:true})
+        data?: T[];
+
+        @Field(()=>Page,{nullable:true})
+        page?: Page;
+    }
+    return Results;
 }
