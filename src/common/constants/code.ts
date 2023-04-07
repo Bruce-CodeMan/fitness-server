b302bf30c840d8984955dd6bcab7044aae00f872
Bruce-CodeMan <1033684650@qq.com>
2023-04-07 10:38:12 +0800

    modified the code

--- src/common/constants/code.ts
+++ src/common/constants/code.ts
@@ -7,3 +7,7 @@ export const SUCCESS = 200;
 export const CODE_NOT_EXPIRE = 10001;
 export const UPDATE_ERR = 10002;
 export const CREATE_ERR = 10003;
+export const ACCOUNT_NOT_EXIST = 10004;
+export const CODE_NOT_EXIST = 10005;
+export const CODE_EXPIRE = 10006;
+export const LOGIN_ERR = 10007;
--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -8,6 +8,7 @@ import { AuthService } from "./auth.service";
 import { UserService } from "../user/user.service";
 import * as dayjs from "dayjs";
 import { Result } from "src/common/dto/result.type";
+import { ACCOUNT_NOT_EXIST, CODE_EXPIRE, CODE_NOT_EXIST, LOGIN_ERR, SUCCESS } from "src/common/constants/code";
 
 @Resolver()
 export class AuthResolver {
@@ -21,24 +22,39 @@ export class AuthResolver {
         return await this.authService.sendCodeMsg(tel);
     }
 
-    @Mutation(() => Boolean, { description: "登录" })
+    @Mutation(() => Result, { description: "登录" })
     async login(
         @Args('tel') tel: string, 
         @Args('code') code: string
-    ): Promise<boolean> {
+    ): Promise<Result> {
         const user = await this.userService.findByTel(tel)
         if(!user) {
-            return false;
+            return {
+                code: ACCOUNT_NOT_EXIST,
+                message: "account is not exsited"
+            };
         }
         if (!user.codeCreatetime || !user.code){
-            return false;
+            return {
+                code: CODE_NOT_EXIST,
+                message: "code is not existed"
+            };
         }
         if(dayjs().diff(dayjs(user.codeCreatetime)) > 60 * 60 * 1000) {
-            return false;
+            return {
+                code: CODE_EXPIRE,
+                message: "code expired"
+            };
         }
         if (user.code === code) {
-            return true;
+            return {
+                code: SUCCESS,
+                message: "login success"
+            };
         }
-        return false;
+        return {
+            code: LOGIN_ERR,
+            message: "login ERR, phone number or code is wrong"
+        };
     }
 }
