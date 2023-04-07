478d7af438b15f7f88c371bf1d14d09f16bec2da
Bruce-CodeMan <1033684650@qq.com>
2023-04-07 13:53:30 +0800

    register JwtModule

---
+++ src/common/constants/jwt.ts
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2023-04-07 13:49:02
+ * @Author: Bruce
+ * @Description: 
+ */
+export const JWT_SECRET = 'BruseHsu'
+export const JWT_EXPIRATION = '60s'
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,13 +5,22 @@
  */
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
 import { AuthResolver } from "./auth.resolver";
 import { AuthService } from "./auth.service";
 import { User } from "../user/models/user.entity";
 import { UserService } from "../user/user.service";
+import { JWT_EXPIRATION, JWT_SECRET } from "@/common/constants/jwt";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [
+        JwtModule.register({
+            secret: JWT_SECRET,
+            signOptions: {
+                expiresIn: JWT_EXPIRATION
+            }
+        }),
+        TypeOrmModule.forFeature([User])],
     providers: [AuthService, AuthResolver, UserService],
     exports: []
 })
