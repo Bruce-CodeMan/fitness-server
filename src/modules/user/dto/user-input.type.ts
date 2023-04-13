0b10058e0a254ea24cc4881b32db604037e1a912
Bruce-CodeMan <1033684650@qq.com>
2023-04-13 13:18:59 +0800

    modified the user-input.type.ts

--- src/modules/user/dto/user-input.type.ts
+++ src/modules/user/dto/user-input.type.ts
@@ -7,8 +7,12 @@ import { Field, InputType } from "@nestjs/graphql";
 
 @InputType()
 export class UserInput{
-    @Field()
+    @Field({ description: "昵称" })
     name: string;
-    @Field()
+
+    @Field({ description: "简介" })
     desc: string;
+
+    @Field({ description: "头像" })
+    avatar: string;
 }
