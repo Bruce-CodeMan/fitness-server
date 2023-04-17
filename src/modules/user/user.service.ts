4cc230a10aadc54c37dd9f9294c95d9858f7f62c
Bruce <1033684650@qq.com>
2023-04-17 22:32:03 +0800

    add comment user.service.ts

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -15,7 +15,11 @@ export class UserService {
     @InjectRepository(User) private UserRepository: Repository<User>,
   ) {}
 
-  // 新增一个用户
+  /**
+   * 新增一个用户
+   * @param entity 传入一个User的实体
+   * @returns Boolean
+   */
   async create(entity: DeepPartial<User>): Promise<boolean> {
     const res = await this.UserRepository.insert(entity);
     if(res && res.raw.affectedRows > 0) {
@@ -24,7 +28,11 @@ export class UserService {
     return false;
   }
 
-  // 删除一个用户
+  /**
+   * 删除一个用户
+   * @param id 传入一个User 的ID
+   * @returns Boolean
+   */
   async del(id: string): Promise<boolean> {
       const res = await this.UserRepository.delete(id);
       if(res.affected > 0) {
@@ -33,7 +41,12 @@ export class UserService {
       return false;
   }
 
-  // 更新一个用户
+  /**
+   * 更新一个用户
+   * @param id 传入一个User 的ID
+   * @param entity 传入一个User 的实体
+   * @returns Boolean
+   */
   async update(id: string, entity: DeepPartial<User>): Promise<boolean> {
       const res = await this.UserRepository.update(id, entity);
       if(res.affected > 0) {
@@ -42,7 +55,11 @@ export class UserService {
       return false;
   }
 
-  // 查询一个用户
+  /**
+   * 查询一个用户
+   * @param id 传入一个ID
+   * @returns 返回一个User 的实体
+   */
   async findOne(id: string): Promise<User> {
       const res = await this.UserRepository.findOne({
           where: {
@@ -52,7 +69,11 @@ export class UserService {
       return res;
   }
 
-  // 通过手机号去查询一个用户
+  /**
+   * 通过手机号去查询一个用户
+   * @param tel 传入一个手机号
+   * @returns 返回一个User 的实体
+   */
   async findByTel(tel: string): Promise<User> {
       const res = await this.UserRepository.findOne({
           where: {
@@ -62,7 +83,12 @@ export class UserService {
       return res;
   }
 
-  // 添加验证码
+  /**
+   * 添加验证码
+   * @param id 传入一个User 的ID
+   * @param code 传入一个 code
+   * @returns Boolean
+   */
   async updateCode(id: string, code: string): Promise<boolean> {
       const res = await this.UserRepository.update(id, { code, codeCreatetime: new Date() });
       if(res.affected > 0) {
