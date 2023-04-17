1908c6a0f506e77aa380b6e0e32fc50d0dae3fd2
Bruce-CodeMan <1033684650@qq.com>
2023-04-17 10:36:34 +0800

    modified the user.entity.ts code style

--- src/modules/user/models/user.entity.ts
+++ src/modules/user/models/user.entity.ts
@@ -11,53 +11,29 @@ export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column({
-        comment: '昵称',
-        default: ''
-    })
+    @Column({ comment: '昵称', default: '' })
     @IsNotEmpty()
     name: string;
 
-    @Column({
-        comment: "描述信息",
-        default: ''
-    })
+    @Column({ comment: "描述信息", default: '' })
     desc: string;
 
-    @Column({
-        comment: "头像",
-        default: ''
-    })
+    @Column({ comment: "头像", default: '' })
     avatar: string;
 
-    @Column({
-        comment: "手机号",
-        nullable: true
-    })
+    @Column({ comment: "手机号", nullable: true })
     tel: string;
 
-    @Column({
-        comment: "密码",
-        nullable: true
-    })
+    @Column({ comment: "密码", nullable: true })
     password: string;
 
-    @Column({
-        comment: "账户信息",
-        nullable: true
-    })
+    @Column({ comment: "账户信息", nullable: true })
     account: string;
 
-    @Column({
-        comment: "验证码",
-        nullable: true
-    })
+    @Column({ comment: "验证码", nullable: true })
     code: string;
 
-    @Column({
-        comment: "验证码生成时间",
-        nullable: true
-    })
+    @Column({ comment: "验证码生成时间", nullable: true })
     codeCreatetime: Date;
 }
 
