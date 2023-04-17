24cee98c216e4af576b55c9e83a02aa5ce33c52c
Bruce-CodeMan <1033684650@qq.com>
2023-04-17 10:38:27 +0800

    modified the student.entity.ts code style

--- src/modules/student/modules/student.entity.ts
+++ src/modules/student/modules/student.entity.ts
@@ -11,32 +11,19 @@ export class Student {
     @PrimaryGeneratedColumn("uuid")
     id: string
 
-    @Column({
-        comment: "昵称",
-        default: ""
-    })
+    @Column({ comment: "昵称", default: "" })
     @IsNotEmpty()
     name: string
 
-    @Column({
-        comment: "手机号",
-        nullable: true
-    })
+    @Column({ comment: "手机号", nullable: true })
     tel: string
 
-    @Column({
-        comment: "头像",
-        nullable: true
-    })
+    @Column({ comment: "头像", nullable: true })
     avatar: string
 
-    @Column({
-        comment: "密码",
-    })
+    @Column({ comment: "密码", })
     password: string
 
-    @Column({
-        comment: "账号",
-    })
+    @Column({ comment: "账号", })
     account: string
 }
