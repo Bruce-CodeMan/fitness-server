10a688537e93eeedc97b8a63a57b2e97172c40be
Bruce <1033684650@qq.com>
2023-04-17 21:44:40 +0800

    create orgImage.entity

---
+++ src/modules/orgImage/models/orgImage.entity.ts
@@ -0,0 +1,21 @@
+import { Organization } from "@/modules/organization/models/organization.entity";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ *  资源机构
+ */
+
+@Entity('org_image')
+export class OrgImage {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ type: 'text', comment: '地址' })
+    @IsNotEmpty()
+    url: string;
+
+    // 备注信息
+    @Column({ comment: 'remark', nullable: true })
+    remark: string;
+}
