af9a90eec9aa9b5da3e22411de6e22d41ca0500e
Bruce-CodeMan <1033684650@qq.com>
2023-04-18 15:06:45 +0800

    create file to delete the orgImage

--- src/modules/orgImage/orgImage.service.ts
+++ src/modules/orgImage/orgImage.service.ts
@@ -14,6 +14,28 @@ export class OrgImageService {
         @InjectRepository(OrgImage)
         private readonly orgImageRepository: Repository<OrgImage>
     ){}
-
     
+    /**
+     * 通过传入的id删除orgImage的信息
+     * @param id organization的ID
+     * @returns 
+     */
+    async deleteByOrg(id): Promise<boolean> {
+        // 自定义查询
+        const images = await this.orgImageRepository.createQueryBuilder('orgImage')
+                                     .where(`orgImage.orgIdForFrontId = '${id}'`)
+                                     .orWhere(`orgImage.orgIdForRoomId = '${id}'`)
+                                     .orWhere(`orgImage.orgIdForOtherId = '${id}'`)
+                                     .getMany();
+        if(images.length === 0) {
+            return true
+        }
+        const delResult = await this.orgImageRepository.delete(
+            images.map((item) => item.id)
+        );
+        if(delResult.affected > 0) {
+            return true;
+        }
+        return false;
+    }
 }
