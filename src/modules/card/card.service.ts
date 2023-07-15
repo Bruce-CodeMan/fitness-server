95610f4e9ae4ee0310ef44d4726d1580abfdf49d
Bruce <1033684650@qq.com>
2023-07-15 21:59:31 +0800

    create the card.service.ts

--- src/modules/card/card.service.ts
+++ src/modules/card/card.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { DeepPartial, Repository } from "typeorm";
+import { DeepPartial, FindOptionsWhere, Repository } from "typeorm";
 
 
 // Custom Impors
@@ -35,4 +35,41 @@ export class CardService {
     }
 
     // 通过id更新消费卡
+    async updateById(id: string, entity: DeepPartial<Card>): Promise<boolean> {
+        const existEntity = await this.findById(id);
+        if(!existEntity) {
+            return false;
+        }
+        Object.assign(existEntity, entity)
+        const res = await this.cardRepository.save(existEntity);
+        if(res) {
+            return true;
+        }
+        return false
+    }
+
+    // 查询消费卡
+    async findCards({where}: {where: FindOptionsWhere<Card>}): Promise<[Card[], number]> {
+        return this.cardRepository.findAndCount({
+            where,
+            order: {
+                createdAt: 'DESC'
+            },
+            relations: ['course']
+        })
+    }
+
+    // 通过id删除消费卡
+    async deleteById(id: string, userId: string): Promise<boolean> {
+        const res_1 = await this.cardRepository.update(id, {
+            deletedBy: userId,
+        })
+        if(res_1) {
+            const res = await this.cardRepository.softDelete(id);
+            if(res.affected > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
